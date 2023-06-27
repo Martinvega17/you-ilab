5b9443dc23e5704e9082b00e253415fa09f29805
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-06-27 00:58:13 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ function App() {
 
     <div className="App">
     <Nav />
-
-    
+    <Container_image />
   </div>
 );
 }
--- src/components/container_image.jsx
+++ src/components/container_image.jsx
@@ -7,53 +7,9 @@ import logo_ipicyt from '../assets/logo-ipicyt.png';
 
 const Nav = () => {
     return (
-        <nav>
-            <div className="bg-azul-claro">
-                <div className="container mx-auto px-4 py-2 md:py-1 flex items-center justify-between ml-1">
-                    <div className="flex items-center">
-                    <img className="text-white text-lg font-semibold h-20 w-25 ml-1" src={logo_you_i_lab} alt="Logo" />
-                    </div>
-                    <ul className="navbar-nav hidden md:flex space-x-8 ml-12">
-                        <li className="nav-item me-3">
-                            <a className="nav-link text-negro-claro hover:text-azul-dark-800 text-lg hover:underline" href="#Inicio">Home</a>
-                        </li>
-                        <li className="nav-item me-3">
-                            <a className="nav-link text-negro-claro hover:text-azul-dark-800 text-lg hover:underline" href="#About">About us</a>
-                        </li>
-                        <li className="nav-item me-3">
-                            <a className="nav-link text-negro-claro over:text-azul-dark-800 text-lg hover:underline" href="#Projects">Projects</a>
-                        </li>
-                        <li className="nav-item me-3">
-                        <a className="nav-link text-negro-claro over:text-azuk-darl-800 text-lg hover:underline" href="#Contact">Contact</a>
-                        </li>
-                    </ul>
-                    <div className="absolute top-0 right-0 flex items-end justify-end mt-3 mr-5 hidden lg:flex">
-                        <div className="contents">
-                            <img src={logo_cns} alt="logo cns" className="h-20 ml-2" />
-                            <img src={logo_ipicyt} alt="logo ipicyt" className="h-20 absolute left-0" style={{ marginLeft: "-110px" }} />
-                        </div>
-                    </div>
-
-                    <div className="relative">
-                        <div className="dropdown inline-block">
-                            <button className="bg-white text-gray-900 font-semibold py-2 px-4 rounded inline-flex items-center">
-                                <span id="currentLanguage" className="mr-1">Español</span>
-                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M6 8l4 4 4-4" />
-                                </svg>
-                            </button>
-                            <ul id="languageMenu" className="dropdown-menu absolute hidden text-gray-700 pt-1">
-                                <li className=""><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#" onClick="langHandler('Español')">Español</a></li>
-                                <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#" onClick="langHandler('1')">English</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div id="menuContainer" className="hidden md:block">
-                    <hr className="border-gray-700 my-2" />
-                </div>
-            </div>
-        </nav>
+        <div className="">
+            <img src="" alt="" />
+        </div>
     );
 };
 
