cc762fe93ca00824dd0c8d92083ed4ab4e569e0d
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-07-01 11:55:38 -0600

    commit

--- src/components/carrusel.jsx
+++ src/components/carrusel.jsx
@@ -27,8 +27,8 @@ const Carrusel = () => {
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
