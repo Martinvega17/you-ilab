a094f54f4094d0504195b29403a802c35c599220
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-07-01 04:03:43 -0600

    import libs

--- src/components/carrusel.jsx
+++ src/components/carrusel.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import centinela from '../assets/proyectos/logo_centinela.png';
 import wenet from '../assets/proyectos/logo_wenet.png';
@@ -9,46 +11,98 @@ import muframex from '../assets/proyectos/logo_muframex.png';
 import horizonte from '../assets/proyectos/logo_hsaludable.png';
 import juventud from '../assets/proyectos/logo_juventud.png';
 
-
 const Carrusel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: true, // Reproducción automática
+    autoplaySpeed: 3000, // Velocidad de reproducción automática (en milisegundos)
+  }
+
+  const responsiveSettings = [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ];
+
+  const images = [
+    {
+      id: 1,
+      src: centinela,
+      alt: "Logo Centinela",
+      url: "https://www.example.com/centinela"
+    },
+    {
+      id: 2,
+      src: wenet,
+      alt: "Logo WeNet",
+      url: "https://www.example.com/wenet"
+    },
+    {
+      id: 3,
+      src: centinela_salud,
+      alt: "Logo Centinela Salud",
+      url: "https://www.example.com/centinela-salud"
+    },
+    {
+      id: 4,
+      src: muframex,
+      alt: "Logo Muframex",
+      url: "https://www.example.com/muframex"
+    },
+    {
+      id: 5,
+      src: horizonte,
+      alt: "Logo HSaludable",
+      url: "https://www.example.com/hsaludable"
+    },
+    {
+      id: 6,
+      src: juventud,
+      alt: "Logo Juventud",
+      url: "https://www.example.com/juventud"
+    }
+  ];
+
   return (
     <div>
-      <h2 className="text-center text-4xl mb-5 mt-3">Projects</h2>
-      <Carousel showStatus={false} showThumbs={false} autoPlay infiniteLoop transitionTime={500} className="rtl">
-        <div className="carousel-image">
-          <a href="https://google.com" target='_blanck'>
-            <img src={centinela} alt="Centinela" className="max-h-40 object-contain" />
-          </a>
-        </div>
-        <div className="carousel-image">
-          <a href="https://example.com">
-            <img src={wenet} alt="Wenet" className="max-h-40 object-contain" />
-          </a>
-        </div>
-        <div className="carousel-image">
-          <a href="https://example.com">
-            <img src={centinela_salud} alt="Centinela de la salud" className="max-h-40 object-contain" />
-          </a>
-        </div>
-        <div className="carousel-image">
-          <a href="https://example.com">
-            <img src={muframex} alt="Muframex" className="max-h-40 object-contain" />
-          </a>
-        </div>
-        <div className="carousel-image">
-          <a href="https://example.com">
-            <img src={horizonte} alt="Horizonte saludable" className="max-h-40 object-contain" />
-          </a>
-        </div>
-        <div className="carousel-image">
-          <a href="https://example.com">
-            <img src={juventud} alt="Juventud al dia" className="max-h-40 object-contain" />
-          </a>
-        </div>
-
-      </Carousel>
+      <h2 className='text-center'>Proyectos</h2>
+      <Slider {...settings} responsive={responsiveSettings}>
+        {images.map((image) => (
+          <div key={image.id}>
+            <a href={image.url} target="_blank" rel="noopener noreferrer">
+              <img src={image.src} alt={image.alt} className='carrusel-images' />
+            </a>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
 
-export default Carrusel;
+export default Carrusel
