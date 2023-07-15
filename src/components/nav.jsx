b84138082b77f1e213ffeab2353f797e7afe84a9
Martin Vega Perez <martinvega0313@gmail.com>
2023-07-15 15:24:17 -0600

    first commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
-import { RiMenuLine, RiCloseLine } from 'react-icons/ri'; // Importar los iconos necesarios
+import { RiMenuLine, RiCloseLine } from 'react-icons/ri';
 import logo_you_i_lab from '../assets/logos/logo-you-i_lab.png';
 import logo_cns from '../assets/logos/logo-cns.png';
 import logo_ipicyt from '../assets/logos/logo-ipicyt.png';
@@ -8,135 +8,140 @@ import styled from 'styled-components';
 import BurguerButton from './BurguerButton';
 
 const Nav = () => {
-    const location = useLocation();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const checkWindowSize = () => {
+      setIsMobile(window.innerWidth <= 768);
     };
 
-    useEffect(() => {
-        const checkWindowSize = () => {
-            setIsMobile(window.innerWidth <= 768);
-        };
+    checkWindowSize();
 
-        checkWindowSize();
+    window.addEventListener('resize', checkWindowSize);
 
-        window.addEventListener('resize', checkWindowSize);
+    return () => {
+      window.removeEventListener('resize', checkWindowSize);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener('resize', checkWindowSize);
-        };
-    }, []);
-
-    return (
-        <nav className={`relative bg-azul-claro ${isMenuOpen ? 'bg-azul-claro' : ''}`}>
-            <div className="mx-auto px-8 py-3 md:py-1">
-                <div className="flex items-center justify-center">
-                    <div className="mr-auto">
-                        <Link to="/you-ilab/" className="contents">
-                            <img
-                                className={`text-white text-lg font-semibold h-20 ${!isMenuOpen ? 'md:ml-0' : ''}`}
-                                src={logo_you_i_lab}
-                                alt="Logo"
-                                style={{ maxWidth: '120px' }}
-                            />
-                        </Link>
-                    </div>
-                    {isMobile ? (
-                        <div className="fixed top-0 right-0">
-                            {/* Botón */}
-                            <button className="md:hidden text-black-800 font-black text-4xl mr-2 mt-2" onClick={toggleMenu}>
-                                {isMenuOpen ? <RiCloseLine /> : <RiMenuLine />}
-                            </button>
-                        </div>
-                    ) : (
-                        <div className="hidden md:flex items-center justify-center flex-grow">
-                            <ul className="flex justify-center text-right">
-                                <li className="nav-item mr-8">
-                                    <NavLink
-                                        to="/you-ilab/"
-                                        className={({ isActive }) =>
-                                            isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
-                                        }
-                                        activeClassName="active"
-                                    >
-                                        Inicio
-                                    </NavLink>
-                                </li>
-                                <li className="nav-item mr-8">
-                                    <NavLink
-                                        to="/you-ilab/about"
-                                        className={({ isActive }) =>
-                                            isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
-                                        }
-                                        activeClassName="active"
-                                    >
-                                        Nosotros
-                                    </NavLink>
-                                </li>
-                                <li className="nav-item mr-8">
-                                    <NavLink
-                                        to="/you-ilab/projects"
-                                        className={({ isActive }) =>
-                                            isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
-                                        }
-                                        activeClassName="active"
-                                    >
-                                        Proyectos
-                                    </NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink
-                                        to="/you-ilab/contact"
-                                        className={({ isActive }) =>
-                                            isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
-                                        }
-                                        activeClassName="active"
-                                    >
-                                        Contacto
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-                </div>
+  return (
+    <nav className={`relative bg-azul-claro ${isMenuOpen ? 'bg-azul-claro' : ''}`}>
+      <div className="mx-auto px-8 py-3 md:py-1">
+        <div className="flex items-center justify-center">
+          <div className="mr-auto">
+            <Link to="/you-ilab/" className="contents">
+              <img
+                className={`text-white text-lg font-semibold h-20 ${!isMenuOpen ? 'md:ml-0' : ''}`}
+                src={logo_you_i_lab}
+                alt="Logo"
+                style={{ maxWidth: '120px' }}
+              />
+            </Link>
+          </div>
+          {isMobile ? (
+            <div className="fixed top-0 right-0">
+              <button className="md:hidden text-black-800 font-black text-4xl mr-2 mt-2" onClick={toggleMenu}>
+                {isMenuOpen ? <RiCloseLine /> : <RiMenuLine />}
+              </button>
             </div>
-            {isMenuOpen && isMobile && (
-                <NavContainer>
-                    <div className="fixed inset-0 flex items-center justify-center">
-
-                        <div className={`links ${isMenuOpen ? 'active' : ''}`}>
-                            <a className="mb-12" onClick={toggleMenu} href="/you-ilab/">Home</a>
-                            <a className="mb-12" onClick={toggleMenu} href="/you-ilab/about">About</a>
-                            <a className="mb-12" onClick={toggleMenu} href="/you-ilab/projects">Projects</a>
-                            <a className="mb-12" onClick={toggleMenu} href="/you-ilab/contact">Contact</a>
-                        </div>
-                        <div className='burguer'>
-                            <BurguerButton clicked={isMenuOpen} handleClick={toggleMenu} />
-                        </div>
-                        <BgDiv className={`initial ${isMenuOpen ? ' active' : ''}`}></BgDiv>
-                    </div>
-                </NavContainer>
-            )}
-            <div className="container mx-auto px-4 py-0 md:py-0">
-                <div className="absolute top-0 right-0 flex items-end justify-end mt-0 mr-0">
-                    <div className="contents">
-                        <img src={logo_cns} alt="logo cns" className="h-20 ml-2 md:block hidden" id="cns" />
-                        <img
-                            src={logo_ipicyt}
-                            alt="logo ipicyt"
-                            className="h-20 absolute left-0"
-                            id="logo-ipicyt"
-                            style={{ marginLeft: '-110px' }}
-                        />
-                    </div>
-                </div>
-
+          ) : (
+            <div className="hidden md:flex items-center justify-center flex-grow">
+              <ul className="flex justify-center text-right">
+                <li className="nav-item mr-8">
+                  <NavLink
+                    to="/you-ilab/"
+                    className={({ isActive }) =>
+                      isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
+                    }
+                    activeClassName="active"
+                  >
+                    Inicio
+                  </NavLink>
+                </li>
+                <li className="nav-item mr-8">
+                  <NavLink
+                    to="/you-ilab/about"
+                    className={({ isActive }) =>
+                      isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
+                    }
+                    activeClassName="active"
+                  >
+                    Nosotros
+                  </NavLink>
+                </li>
+                <li className="nav-item mr-8">
+                  <NavLink
+                    to="/you-ilab/projects"
+                    className={({ isActive }) =>
+                      isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
+                    }
+                    activeClassName="active"
+                  >
+                    Proyectos
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/you-ilab/contact"
+                    className={({ isActive }) =>
+                      isActive ? 'text-red-500 font-bold text-lg underline' : 'text-blue-800 font-bold text-lg'
+                    }
+                    activeClassName="active"
+                  >
+                    Contacto
+                  </NavLink>
+                </li>
+              </ul>
             </div>
-        </nav>
-    );
+          )}
+        </div>
+      </div>
+      {isMenuOpen && isMobile && (
+        <NavContainer>
+          <div className="fixed inset-0 flex items-center justify-center">
+            <div className={`links ${isMenuOpen ? 'active' : ''}`}>
+              <Link className="mb-12" onClick={toggleMenu} to="/you-ilab/">
+                Home
+              </Link>
+              <Link className="mb-12" onClick={toggleMenu} to="/you-ilab/about">
+                About
+              </Link>
+              <Link className="mb-12" onClick={toggleMenu} to="/you-ilab/projects">
+                Projects
+              </Link>
+              <Link className="mb-12" onClick={toggleMenu} to="/you-ilab/contact">
+                Contact
+              </Link>
+            </div>
+            <div className="burguer">
+              <BurguerButton clicked={isMenuOpen} handleClick={toggleMenu} />
+            </div>
+            <BgDiv className={`initial ${isMenuOpen ? ' active' : ''}`}></BgDiv>
+          </div>
+        </NavContainer>
+      )}
+      <div className="container mx-auto px-4 py-0 md:py-0">
+        <div className="absolute top-0 right-0 flex items-end justify-end mt-0 mr-0">
+          <div className="contents">
+            <img src={logo_cns} alt="logo cns" className="h-20 ml-2 md:block hidden" id="cns" />
+            <img
+              src={logo_ipicyt}
+              alt="logo ipicyt"
+              className="h-20 absolute left-0"
+              id="logo-ipicyt"
+              style={{ marginLeft: '-110px' }}
+            />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
